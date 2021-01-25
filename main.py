b7d9158eadbec491f92b7106838441cefb1fc9c7
Ish <77935410+ishmachine@users.noreply.github.com>
2021-01-24 23:37:33 -0500

    Create main.py
    This is the main file.

---
+++ main.py
@@ -0,0 +1,17 @@
+import ptm_functions as ptm
+
+# Instead of using if-else, this handy dict
+# does the same thing, and it's more elegant
+func_equiv = {
+    "new": ptm.new_task,
+    "edit": ptm.edit_task,
+    "del": ptm.del_task,
+    "done": ptm.mark_done
+}
+
+ptm.display()
+while True:
+    cmd = ptm.valid_command()
+    func_equiv[cmd[0]](cmd)
+    ptm.cls()
+    ptm.display()
